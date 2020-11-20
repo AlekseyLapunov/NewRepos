eec5c46da38020aa37852b3bc619ef7e59486d10
AlekseyLapunov <lapunoff.alex@yandex.ru>
2020-11-20 13:36:42 +0700

    fast pow

---
+++ main2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+double my_pow_fast(double num, unsigned int deg)
+{
+	double result = 1;
+	if (!deg) return 1;
+	// Если степень положительная
+	while (deg)
+	{
+		if (deg % 2 == 0)
+		{
+			deg /= 2;
+			num *= num;
+		}
+		else
+		{
+			deg--;
+			result *= num;
+		}
+	}
+	return result;
+}
+
+int main()
+{
+	double num;
+	int	deg;
+	cin >> num;
+	cin >> deg;
+	cout.precision(6);
+	cout << "> " << my_pow_fast(num, deg);
+	return 0;
+}
